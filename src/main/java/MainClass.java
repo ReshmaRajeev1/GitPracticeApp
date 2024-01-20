4f4491b655d03646e3f56eca21bbe0e9d1c8fb96
ReshmaRajeev1 <youremail@yourdomain.com~>
2024-01-20 06:13:13 +0530

    git erge by command line

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,5 @@
 public class MainClass {
     public static void main(String args[]){
-        System.out.println("featre branch and main commit");
+        System.out.println("Git Merge by command line");
     }
 }
